4c8673891bea13889a748a57fdde82c604c69725
strc12 <ric@oundleschool.org.uk>
2024-11-28 10:36:12 +0000

    test

--- subjects.php
+++ subjects.php
@@ -10,6 +10,16 @@
   
   <input type="submit" value="Add Subject">
 </form>
-  
+<?php
+    include_once("connection.php");
+    $stmt = $conn->prepare("SELECT * FROM tblsubjects");
+    $stmt->execute();
+    while ($row=$stmt->fetch(PDO::FETCH_ASSOC))
+        {
+            #print_r($row);
+            echo($row["subjectname"]." taught by ".$row["teacher"]."<br>");
+        }
+
+    ?>
 </body>
 </html>
